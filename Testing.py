990fd6abd80d49fecb0f2643febc92b392909149
Alex Nino <38741171+AlexxNino@users.noreply.github.com>
2020-03-04 12:39:48 -0600

    Add files via upload

--- Testing.py
+++ Testing.py
@@ -1,19 +1,35 @@
-#
-#This is just UI testing in python for learning purposes
-#   -Alex
+user='root'
+password='Al102030!'  #CHANGE THIS
+host='ANMACAIR.local'
+database='Test'
+
+
 
 import sys
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QLineEdit
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtCore import QSize    
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host=host,
+  user=user,
+  passwd=password,
+  database='sys'  #specify database 
+)
+
+#mycursor.execute("CREATE TABLE Courses (CRN INT(5))")
+#mycursor.execute("CREATE TABLE Courses (CRN INT(5),Course_Name VARCHAR(45))")gl
+#val=''
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
         self.setMinimumSize(QSize(320, 140))    
-        self.setWindowTitle("PyQt Line Edit example (textfield) - pythonprogramminglanguage.com") 
+        self.setWindowTitle("Testing Py connector and buttons") 
 
         self.nameLabel = QLabel(self)
         self.nameLabel.setText('CRN:')
@@ -30,7 +46,15 @@ class MainWindow(QMainWindow):
 
     def clickMethod(self):
         print('Test line edit input = ' + self.line.text())
-        #this is where I add the python connector
+        #global val
+        val = (self.line.text())
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO Courses (CRN) VALUES ('%s')"
+        mycursor.execute(sql, val)
+        #Something is wrong here
+
+
+mydb.commit()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
