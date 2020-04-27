f459bcc2f27a9c61d976d78f2dcb9f892e9ff953
Alex Nino <38741171+AlexxNino@users.noreply.github.com>
2020-04-27 02:04:19 -0500

    Student views
    Most is running just need to fix gpa to semester only

--- CS3321_Arashid/studentwindow.h
+++ CS3321_Arashid/studentwindow.h
@@ -17,10 +17,20 @@ class StudentWindow : public QDialog
 public:
     explicit StudentWindow(QWidget *parent = nullptr);
     ~StudentWindow();
+    void get_semester_gpa();
     void createGlobal(QString, QString);
     void loadcourses();
+    void loadAllClasses();
+    void addClass(QString);
+    void dropClass(QString);
+    void loadStudent();
+    void loadGrades();
+
+
 private slots:
     void on_pushButton_LoadTable_clicked();
+    void on_pushButton_Add_clicked();   //add course
+    void on_pushButton_Drop_clicked();  //drop course
 
 private:
     Ui::StudentWindow *ui;
